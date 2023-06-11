e6b7714f36f59f0603e1bc283da60c1fb99e1a2c
Hazal Yilmaz <hazal.yilmaz@mahrek.com.tr>
2023-06-11 18:06:38 +0300

    twoSum commit

---
+++ other/twoSum.js
@@ -0,0 +1,15 @@
+var twoSum = function(nums, target) {
+    var map = {};
+    for(var i = 0 ; i < nums.length ; i++){
+        var n = nums[i];
+
+        if(map[target-n] >= 0){
+            return [map[target-n],i]
+        }
+        else {
+            map[n] = i;   //use map to store the value and index position
+        }
+    }
+};
+
+a([10, 20], 2);
